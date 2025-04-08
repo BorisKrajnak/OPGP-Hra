08f2a0af117f1c16903427cb9e380a1ec2fdf5e7
Boris Krajňak <boriskrajnak@gmail.com>
2025-04-08 12:43:31 +0200

    Pridal som názov a tlačidlo ktoré bude odkazovať na nastavenia #5

--- uvodne_okno.py
+++ uvodne_okno.py
@@ -1,3 +1,5 @@
+import subprocess
+
 import pygame
 import sys
 
@@ -12,27 +14,42 @@ pygame.display.set_caption("Vesmírna hra")
 # Načítanie obrázka
 try:
     background_img = pygame.image.load(r"img/pozadie_uvodne_okno.jpg")
-    background_img = pygame.transform.scale(background_img, (int(background_img.get_width() * 0.8), int(background_img.get_height() * 0.8)))
+    background_img = pygame.transform.scale(background_img, (int(background_img.get_width()), int(background_img.get_height())))
     screen.blit(background_img, ((width - background_img.get_width()) // 2, (height - background_img.get_height()) // 4))
 except Exception as e:
     print(f"Chyba pri načítaní obrázka: {e}")
 
-# Tlačidlo ukončiť
+# TLACIDLA
 button_width = 250
 button_height = 50
 border_radius = 20
 
-quit_button_rect = pygame.Rect(40, height - button_height - 40, button_width, button_height)
 button_color = (169,169,169) # Sivá
-pygame.draw.rect(screen, button_color, quit_button_rect, border_radius = border_radius)# Zaoblene rohy
+font = pygame.font.SysFont("Arial", 40, bold=True) #Font
 
-# Nastavenie fontu a bilej farby tetxu
-font = pygame.font.SysFont("Arial", 40, bold=True)
-quit_button_text = font.render("UKONČIŤ", True, (255,255,255))
+#Umiestnenie
+quit_button_rect = pygame.Rect(40, height - button_height - 40, button_width, button_height)
+next_button_rect = pygame.Rect(width - button_width - 40, height - button_height - 40, button_width, button_height)
 
-#Zarovnanie tlačidla na stred
+#Pozadie tlacidiel, zaoblenie
+pygame.draw.rect(screen, button_color, quit_button_rect, border_radius = border_radius)
+pygame.draw.rect(screen, button_color, next_button_rect, border_radius=border_radius)
+
+#Text tlacidiel, farba
+quit_button_text = font.render("QUIT", True, (255,255,255))
+next_button_text = font.render("NEXT", True, (255, 255, 255))
+
+#Vycentrovanie textu
 quit_button_text_rect = quit_button_text.get_rect(center = quit_button_rect.center)
+start_button_text_rect = next_button_text.get_rect(center=next_button_rect.center)
+
 screen.blit(quit_button_text, quit_button_text_rect)
+screen.blit(next_button_text, start_button_text_rect)
+
+
+#NADPIS
+title_text = font.render("SPACE RIDER", True, (255, 255, 255))
+screen.blit(title_text, (width // 2 - title_text.get_width() // 2, 50))
 
 
 # Hlavná slučka
@@ -40,17 +57,18 @@ running = True
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            print("Ukončujem hru")
             running = False
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_ESCAPE: # Ukončenie pomocou klávesy ESC
-                print("Ukončujem hru")
                 running = False
         if event.type == pygame.MOUSEBUTTONDOWN:
             if quit_button_rect.collidepoint(event.pos): # Ukončenie pomocou tlačidla
-                print("Ukončujem hru")
                 running = False
-
+            if next_button_rect.collidepoint(event.pos): # Tlačidlo Next
+                subprocess.Popen(["python","nastavenia_hry.py"])
+                running = False
+                pygame.quit()
+                sys.exit()
 
     # Aktualizácia obrazovky
     pygame.display.update()
