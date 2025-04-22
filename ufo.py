5d21e86986fa6ebc2bd3d09f7a5ee857bae9d172
Boris Krajňak <boriskrajnak@gmail.com>
2025-04-22 17:08:35 +0200

    Použil som súbor vyber_pozadia.py v ufo.py #29

--- ufo.py
+++ ufo.py
@@ -1,8 +1,7 @@
 import pygame
-import json
-import os
 import sys
-import random
+
+from vyber_pozadia import nacitaj_pozadie, nacitaj_obrazok
 
 # Inicializácia Pygame
 pygame.init()
@@ -13,40 +12,14 @@ width, height = info.current_w, info.current_h
 screen = pygame.display.set_mode((width, height))
 pygame.display.set_caption("Hra - UFO")
 
-# Načítanie konfiguračného súboru
-with open("game_config.json", "r") as f:
-    config = json.load(f)
-
-map_image_name = config.get("map_image")
-
-# Ak je vybraná posledná mapa (pozadie_vesmir_n12.jpg), vyber náhodnú mapu od n1 po n11
-if map_image_name == "pozadie_vesmir_n12.jpg":
-    random_index = random.randint(1, 11)
-    map_image_name = f"pozadie_vesmir_n{random_index}.jpg"
-
-map_path = os.path.join("img/map_imagines", map_image_name)
-
-# Načítanie mapy alebo fallback
-if os.path.exists(map_path):
-    background = pygame.transform.scale(pygame.image.load(map_path), (width, height))
-else:
-    background = pygame.Surface((width, height))
-    background.fill((0, 0, 0))
-
-# Načítanie obrázka ovládania
-control_image = pygame.image.load("img/ovladanie/ovladanie2.png")
-
-# Zistenie rozmerov obrázka
-control_rect = control_image.get_rect()
-
-# Vypočítanie stredu obrazovky
-control_rect.center = (width // 2, height // 2)
+# Načítanie pozadia a obrázka prostreidku cez import
+background = nacitaj_pozadie("game_config.json",width,height)
+control_image, control_rect = nacitaj_obrazok("img/ovladanie/ovladanie2.png", width // 2, height // 2)
 
 # Hlavný cyklus
 running = True
 while running:
     screen.blit(background, (0, 0))
-
     # Vykreslenie obrázka do stredu obrazovky
     screen.blit(control_image, control_rect)
 
